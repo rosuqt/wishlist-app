de465a4e420d120a264ce4ba4d9af539f39903a4
Allyza Rose <allyzarosecayer@gmail.com>
2024-12-07 15:41:19 +0800

    updated .env

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = 'https://ipbjyyglslqvdrsmmcvl.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwYmp5eWdsc2xxdmRyc21tY3ZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0NzQ0NzgsImV4cCI6MjA0OTA1MDQ3OH0.WJz6PJKb1NO-LfsSZ9Ad_y4VCDWNta9eI6ErPEt4qwI';
+
 const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 app.use(express.static(path.join(__dirname, 'wishlist')));
 app.use(express.json());
